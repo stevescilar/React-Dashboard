41c3bbba53784a62de99b75a2f01d9f60d5e060c
stevescilar <stevemuambi@gmail.com>
2022-08-24 14:42:29 +0300

    day2-update-sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -9,6 +9,9 @@ import { links } from '../data/dummy';
 
 function Sidebar() {
   const activeMenu = true;
+
+  const activeLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounde-lg text-white text-md m-2';
+  const normalLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounde-lg text-md text-gray-700 dark:text-gray-200 dark:hover text-black hover:bg-light-gray m-2';
   return (
     <div className='ml-3 h-screen md:overflow-hidden overflow-auto md:hover:overflow-auto pb-10'>
       { activeMenu && (<>
@@ -22,6 +25,24 @@ function Sidebar() {
               </button>
             </TooltipComponent>
         </div> 
+        <div className='mt-10'>
+          {links.map((item) => (
+            <div key={item.title}>
+              <p className="text-gray-400 m-3 mt-4 uppercase">
+                 { item.title }
+              </p>
+              {item.links.map((link) => (
+                <NavLink to={`/${link.name}`} key={links.name} onClick={() => {}} className={( {isActive }) => 
+                isActive ? activeLink :normalLink}>
+                    {link.icon}
+                  <span className='capitalize'>
+                    {link.name}
+                  </span>
+                </NavLink>
+              ))}
+            </div>
+          ))}
+        </div>
         </>)}
     </div>
   )
